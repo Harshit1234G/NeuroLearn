92efdcc10254968373a5ce1f8cf29a2b18e0e97e
Harshit <harshitkumawat849@gmail.com>
2025-09-29 11:41:24 +0530

    Updated orchestration: supports both CLI architecture and Web architecture

--- backend/agents/orchestration.py
+++ backend/agents/orchestration.py
@@ -3,7 +3,7 @@ from langgraph.checkpoint.memory import MemorySaver
 
 from backend.agents import (
     StudentState,
-    # AssessmentAgent,
+    AssessmentAgent,
     DiagnosticAgent,
     ReportAgent,
     TutorAgent
@@ -13,11 +13,11 @@ from backend.config import MAX_QUES
 
 
 class NeuroLearn:
-    def __init__(self):
+    def __init__(self, *, cli: bool = True):
         self.logger = get_logger(self.__class__.__name__)
+        self.cli = cli
 
         # initialize agents
-        # assessment = AssessmentAgent()
         diagnostic = DiagnosticAgent()
         report = ReportAgent()
         tutor = TutorAgent()
@@ -26,23 +26,28 @@ class NeuroLearn:
         builder = StateGraph(StudentState)
 
         # adding nodes
-        # builder.add_node(assessment.name, assessment.run)
         builder.add_node(diagnostic.name, diagnostic.run)
         builder.add_node(report.name, report.run)
         builder.add_node(tutor.name, tutor.run)
 
         # adding edges and coditional edges
+        if self.cli:
+            assessment = AssessmentAgent()
+            builder.add_node(assessment.name, assessment.run)
 
-        # builder.set_entry_point(assessment.name)
-        # builder.add_conditional_edges(
-        #     assessment.name,
-        #     self._is_all_correct,
-        #     {
-        #         True: report.name,
-        #         False: diagnostic.name
-        #     }
-        # )
-        builder.set_entry_point(diagnostic.name)
+            builder.set_entry_point(assessment.name)
+            builder.add_conditional_edges(
+                assessment.name,
+                self._is_all_correct,
+                {
+                    True: report.name,
+                    False: diagnostic.name
+                }
+            )
+
+        else:
+            builder.set_entry_point(diagnostic.name)
+
         builder.add_edge(diagnostic.name, report.name)
         builder.add_conditional_edges(
             report.name,
@@ -66,13 +71,21 @@ class NeuroLearn:
         return con
     
 
-    def run(self, student_name: str, tags: str) -> StudentState:
+    def _run_cli(self, student_name: str, tags: str) -> StudentState:
         try:
-            state = state = {
+            state = {
                 'name': student_name,
                 'tags': tags,
                 'already_asked': [],
-                'current_difficulty': 2
+                'current_difficulty': 2,
+                'diagnosis': {
+                    "diagnosis": f"{student_name} demonstrates strong performance across all assessment aspects, achieving a perfect score. Their ability to listen, grasp, retain, and apply knowledge is evident, particularly in handling {tags}-related problems, which suggests a solid understanding of concepts.",
+                    "recommendations": f"To maintain and further enhance their skills, {student_name} should engage in more complex problem-solving scenarios that challenge their application of concepts in real-world contexts. Additionally, exploring advanced topics in {tags} and related areas can help deepen their understanding and prepare them for future assessments.",
+                    "listening": 100,
+                    "grasping": 100,
+                    "retention": 100,
+                    "application": 100
+                }
             }
             state = self.graph.invoke(
                 state,
@@ -85,3 +98,14 @@ class NeuroLearn:
         
         except Exception as e:
             self.logger.exception(f'Error during the workflow: {e}')
+
+
+    def _run_web(self, student_name: str, tags: str) -> StudentState:
+        ...
+
+
+    def run(self, student_name: str, tags: str) -> StudentState:
+        if self.cli:
+            return self._run_cli(student_name, tags)
+        
+        return self._run_web(student_name, tags)
