0eb976f10212bd97595dca1c16eee4febcba54d1
Harshit <harshitkumawat849@gmail.com>
2025-09-30 11:23:28 +0530

    Created a PDF saving Utility

--- backend/utils/__init__.py
+++ backend/utils/__init__.py
@@ -1,4 +1,5 @@
 from .caching import save_state, load_state, sanitize_filename
 from .logger import get_logger
+from .pdf_saver import create_pdf
 
-__all__ = [sanitize_filename, save_state, load_state, get_logger]
+__all__ = [sanitize_filename, save_state, load_state, get_logger, pdf_saver]
---
+++ backend/utils/pdf_saver.py
@@ -0,0 +1,17 @@
+import pypandoc
+
+
+def create_pdf(markdown_text: str, output_file: str) -> None:
+    pypandoc.convert_text(
+        markdown_text,
+        to= 'pdf',
+        format= 'md',
+        outputfile= output_file,
+        extra_args= [
+            '--pdf-engine=xelatex',
+            '-V', 'mainfont=Times New Roman',
+            '-V', 'monofont=Courier New',
+            '-V', 'fontsize=12pt',
+            '-V', 'geometry:margin=1in'
+        ]
+    )
